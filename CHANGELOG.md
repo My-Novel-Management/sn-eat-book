6919db251ba337f5eff7f9981ad3c4531885d608
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-07-06 22:01:01 +0900

    Add storybuilder and update src (#1)
    * Add storybuilder
    
    * Fix storybuilder
    
    * Add archive and scene
    
    * Update v1.3.1
    
    * Update storybuilder
    
    * Add info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,12 +6,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
-## [0.0.1] - 2020-XX-XX
+## [1.3.1] - 2020-07-06
 ### Added
 - This CHANGELOG file to hopefully serve as an evolving example of a standardized open source project CHANGELOG.
 - README one line implemented.
+- Archive
 
-<!--Must change link URL-->
 
-[Unreleased]: https://github.com/XXX/compare/v0.0.1...HEAD
-[0.0.1]: https://github.com/XXX/releases/v0.0.1
+[Unreleased]: https://github.com/My-Novel-Management/sn-eat-book/compare/v1.3.1...HEAD
+[1.3.1]: https://github.com/My-Novel-Management/sn-eat-book/releases/v1.3.1
--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ from storybuilder.builder.world import World
 from storybuilder.assets import basic
 from config import ASSET
 # import scenes
-# from scenes import xxx
+from scenes import stage
 
 
 ################################################################
@@ -25,28 +25,48 @@ from config import ASSET
 ################################################################
 
 # Constant
-TITLE = "作品タイトル"
-OUTLINE = "あらすじ"
-MAJOR, MINOR, MICRO = 0, 0, 1
+TITLE = "本を喰む子"
+ONELINE = "本屋の店主から、本を食べてしまうという子の話を聞く"
+OUTLINE = "約6000字のホラー短編。よく利用している本屋である日、あなたは店主から奇妙な子どもの話を聞かされるが"
+MAJOR, MINOR, MICRO = 1, 3, 1
 
 
 # Episodes
-def ep_xxx(w: World):
-    return w.episode('episode_title',
-            outline="description")
+def ep1(w: World):
+    return w.episode('本を読む人',
+            stage.sc_read_book(w),
+            )
 
+def ep2(w: World):
+    return w.episode('本を喰む子',
+            stage.sc_eat_book(w),
+            )
+
+def ep3(w: World):
+    return w.episode('本が消える音',
+            stage.sc_vanish_book(w),
+            )
 
 def ch_main(w: World):
     return w.chapter('main',
+            ep1(w),
+            ep2(w),
+            ep3(w),
+            w.symbol("（了）"),
             )
 
 
 def main(): # pragma: no cover
     w = World.create_world(f"{TITLE}")
+    w.config.set_oneline(f"{ONELINE}")
     w.config.set_outline(f"{OUTLINE}")
     w.config.set_version(MAJOR, MINOR, MICRO)
     w.db.set_from_asset(basic.ASSET)
     w.db.set_from_asset(ASSET)
+    w.config.set_contest_info("エブリスタの妄想コンテスト「一冊の本」優秀作に選出")
+    w.config.set_caution("本作はホラーですので、耐性のない方は読まないことをおすすめします")
+    w.config.set_sites("エブリスタ", "小説家になろう", "ノベルアッププラス", "カクヨム")
+    w.config.set_released(11, 27, 2018)
     return w.run(
             ch_main(w),
             )
